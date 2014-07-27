e088a966c7d5772d3ce6bbae686b902647a33e41
latikamahapatra <latikamahapatra@gmail.com>
2014-07-27 11:28:09 +0530

    Main page
    code

--- akjava/LogicMain.java
+++ akjava/LogicMain.java
@@ -7,7 +7,9 @@ public class LogicMain extends HttpServlet
 	Connection c;
 	Statement stmt;
 	String[] re=new String[50];
-	int[] inc=new int[70];
+	
+	String[] rn=new String[4];
+	int[] inc=new int[4];
 	int[] inn=new int[70];
 	int[] rati=new int[70];
 	public void doGet(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException
@@ -15,19 +17,19 @@ public class LogicMain extends HttpServlet
 		PrintWriter out=res.getWriter();
 		try
 		{
-			out.println("hai");
+			
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			c=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","System","prasanna");
 			stmt=c.createStatement();
 			ResultSet rs1=stmt.executeQuery("select * from recipe");
 			for(int i=0; rs1.next(); i++)
-			{out.println(i);
+			{
 				re[i]=rs1.getString("rid");
+				rn[i]=rs1.getString("rname");
 				
 			}
 			for(int j=0;j<re.length;j++)
 			{
-					out.println(j);
 					//Statement stmt=c.createStatement();
 					ResultSet rs2=stmt.executeQuery("select * from ingredients i1 natural join uses u where u.rid='"+re[j]+"'");
 					for(int k=0; rs2.next(); k++)
@@ -43,23 +45,23 @@ public class LogicMain extends HttpServlet
 			
 			rati[i]=sum(inn)-sum(inc);	
 			}
-			for(int p=0;p<re.length;p++)
+			for(int p=0;p<rn.length;p++)
 			{
-				for(int q=0;q<re.length-1-p;q++)
+				for(int q=0;q<rn.length-1-p;q++)
 				{
 					if(rati[q]>rati[q+1])
 					{
 						int t1=rati[q];
 						rati[q]=rati[q+1];
 						rati[q+1]=t1;
-						String t2=re[q];
-						re[q]=re[q+1];
-						re[q+1]=t2;
+						String t2=rn[q];
+						rn[q]=rn[q+1];
+						rn[q+1]=t2;
 					}
 				}
 			}
 			for(int p=0;p<re.length;p++)
-			out.println(re[p]+rati[p]);
+			out.println(re[p]+"    "+rn[p]+"     "+rati[p]);
 			c.close();
 		}
 		catch(Exception e)
